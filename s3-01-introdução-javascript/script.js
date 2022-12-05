2e0cf774d66f888fe259cb6ccbb2d6dfb2bf51b5
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2022-12-05 10:45:52 -0300

    Introdução javascript

---
+++ s3-01-introdução-javascript/script.js
@@ -0,0 +1,65 @@
+//alert("Importação deu certo");
+
+//VARIAVEIS
+
+
+let thiago = 2; 
+console.log(thiago)
+
+thiago = 3; 
+console.log(thiago)
+
+let a = 5;
+let b = 7;
+let c = 12;
+
+let soma = a+b+c;
+
+console.log(a, b, c);
+
+console.log(soma);
+
+let multiplicacao = a*b*c;
+
+console.log(multiplicacao);
+
+
+//TIPOS DE VARIAVEIS
+
+let tipo_let = 123;
+var tipo_var = 123;
+const tipo_const = 123;
+
+console.log(tipo_let);
+tipo_let = 321;
+console.log(tipo_let);
+
+console.log(tipo_var);
+tipo_var = 321;
+console.log(tipo_var);
+
+// tipo_const = 321;
+// let tipo_let = 456;
+
+var tipo_var = 456;
+
+
+//TIPOS DE VALORES DAS VARIAVEIS
+let numero = 10;           //number
+let numeroFloat = 3.1415;   //number
+let texto = 'Thiago Koman'; //string
+let boleano = true;         //boolean
+
+console.log(numero);
+console.log(numeroFloat);
+console.log(texto);
+console.log(boleano);
+
+let soma2 = numero + numeroFloat;
+console.log('Soma: ',soma2);
+
+let teste = texto + numero;
+console.log('Teste: ',teste);
+
+let teste2;
+teste2 = 'Teste da demo';
